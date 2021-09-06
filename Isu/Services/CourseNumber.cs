73b7788b6861d942708ed38cfaae91c37e071edb
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-06 20:33:29 +0300

    feat: implemented services

---
+++ Isu/Services/CourseNumber.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Isu.Services
+{
+    public class CourseNumber
+    {
+        private readonly int _number;
+
+        public CourseNumber(int number)
+        {
+            _number = number;
+        }
+
+        public int ToInt()
+        {
+            return _number;
+        }
+    }
+}
---
+++ Isu/Services/GroupName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Isu.Entities;
+using Isu.Tools;
+
+namespace Isu.Services
+{
+    public class GroupName
+    {
+        private const int LENGTH = 5;
+        private const string SPECIALIZATION = "M3";
+        private int _number;
+        public GroupName(string groupName)
+        {
+            if (groupName.Length != LENGTH || groupName[..2] != SPECIALIZATION)
+                throw new IsuException();
+            if (!char.IsDigit(groupName[2]))
+                throw new IsuException();
+            Course = new CourseNumber(groupName[2] - '0');
+            if (!int.TryParse(groupName[3..], out _number))
+                throw new IsuException();
+        }
+
+        public CourseNumber Course { get; }
+
+        /// <param name="groupName">Group name in the format M3XYY. Where X - course number, YY - group number.</param>
+        public string GetName()
+        {
+            return SPECIALIZATION + Course.ToInt() + $"{2:_number}";
+        }
+    }
+}
