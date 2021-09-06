01904ac1a8ccc1afa871f61016bc25117ce2c1ce
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-06 20:34:06 +0300

    feat: implemented entities

---
+++ Isu/Entities/Group.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Isu.Services;
+using Isu.Tools;
+
+namespace Isu.Entities
+{
+    public class Group
+    {
+        private readonly uint _maxStudentsCount;
+        private readonly List<Student> _students = new List<Student>();
+
+        public Group(GroupName groupName, uint maxStudentsCount)
+        {
+            Name = groupName;
+            _maxStudentsCount = maxStudentsCount;
+        }
+
+        public GroupName Name { get; }
+        public ReadOnlyCollection<Student> Students => _students.AsReadOnly();
+
+        public void AddStudent(Student student)
+        {
+            if (_students.Count == _maxStudentsCount)
+                throw new IsuException();
+            _students.Add(student);
+        }
+
+        public void DeleteStudent(Student student)
+        {
+            _students.Remove(student);
+        }
+    }
+}
---
+++ Isu/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace Isu.Entities
+{
+    public class Student
+    {
+        public Student(string name, int id, Group group)
+        {
+            Name = name;
+            ID = id;
+            CurrentGroup = group;
+        }
+
+        public string Name { get; }
+        public int ID { get; }
+
+        public Group CurrentGroup { get; set; }
+    }
+}
