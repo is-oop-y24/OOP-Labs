b029cd0bbd67e6f6b72d387c0b4a21c0ac753bc4
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-09 19:50:19 +0300

    feat: implemented GoodSupply

--- Shops/Services/ProductsManagement/GoodsSupply.cs
+++ Shops/Services/ProductsManagement/GoodsSupply.cs
@@ -1,7 +1,25 @@
-﻿namespace Shops
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Shops
 {
     public class GoodsSupply
     {
+        private readonly List<ProductSupply> _products = new List<ProductSupply>();
 
+        public GoodsSupply() { }
+
+        public ReadOnlyCollection<ProductSupply> ProductSupplies => _products.AsReadOnly();
+
+        public void AddProduct(Product product, int quantity, int worth)
+        {
+            _products.Add(new ProductSupply(product, quantity, worth));
+        }
+
+        public void AddProduct(ProductSupply productSupply)
+        {
+            _products.Add(productSupply);
+        }
+        
     }
 }
--- Shops/Services/ProductsManagement/ProductSupply.cs
+++ Shops/Services/ProductsManagement/ProductSupply.cs
@@ -2,6 +2,15 @@ namespace Shops
 {
     public class ProductSupply
     {
-        
+        private int _quantity;
+        private int _worth;
+        private Product Product { get; }
+
+        public ProductSupply(Product product, int quantity, int worth)
+        {
+            _quantity = quantity;
+            _worth = worth;
+            Product = product;
+        }
     }
 }
