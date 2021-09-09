ac8605f76a35f379b782a2b7e14b847206601350
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-09 16:53:46 +0300

    feat: defined ShopManager interface

--- Shops/Services/IShopManager.cs
+++ Shops/Services/IShopManager.cs
@@ -1,7 +1,12 @@
-﻿namespace Shops
+﻿using System.Collections.Generic;
+
+namespace Shops
 {
     public interface IShopManager
     {
-        
+        Shop CreateShop(string shopName, Address shopAddress);
+        List<Shop> FindShop(string shopName);
+        Shop GetShop(ShopId shopId);
+        Shop GetMostProfitableShop(Purchase purchase);
     }
 }
--- Shops/Services/ShopManager.cs
+++ Shops/Services/ShopManager.cs
@@ -1,7 +1,33 @@
-﻿namespace Shops
+﻿using System.Collections.Generic;
+
+namespace Shops
 {
-    public class ShopManager
+    public class ShopManager : IShopManager
     {
-        
+
+        public ShopManager()
+        {
+            
+        }
+
+        public Shop CreateShop(string shopName, Address shopAddress)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Shop> FindShop(string shopName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Shop GetShop(ShopId shopId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Shop GetMostProfitableShop(Purchase purchase)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
