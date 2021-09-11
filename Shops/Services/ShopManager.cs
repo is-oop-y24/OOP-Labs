42d5a613ec237f6b6f7180935b16c0f5e598ad5a
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-11 18:43:10 +0300

    feat: implemented ShopManager

--- Shops/Services/ShopManager.cs
+++ Shops/Services/ShopManager.cs
@@ -1,32 +1,52 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Shops.Tools;
 
 namespace Shops
 {
     public class ShopManager : IShopManager
     {
-        public ShopManager()
-        {
-            throw new System.NotImplementedException();
-        }
+
+        private List<Shop> _shops = new List<Shop>();
+        private int _currentShopId = 1;
+        public ShopManager() { }
 
         public Shop CreateShop(string shopName, Address shopAddress)
         {
-            throw new System.NotImplementedException();
+            var shop = new Shop(shopName, _currentShopId);
+            _shops.Add(shop);
+            return shop;
         }
 
+        // Returns the Shop list because shopName is not unique.
         public List<Shop> FindShop(string shopName)
         {
-            throw new System.NotImplementedException();
+            return _shops
+                .Where(shop => shop.Name == shopName)
+                .Select(shop => shop)
+                .ToList();
         }
 
         public Shop GetShop(ShopId shopId)
         {
-            throw new System.NotImplementedException();
+            return _shops
+                .Where(shop => shop.Id == shopId)
+                .Select(shop => shop)
+                .Single();
         }
 
         public Shop GetMostProfitableShop(Purchase purchase)
         {
-            throw new System.NotImplementedException();
+            Shop shop = _shops
+                .Where(shop => shop.AreProductsEnough(purchase))
+                .OrderBy(shop => shop.CalculateTotalCost(purchase))
+                .Select(shop => shop)
+                .FirstOrDefault();
+
+            if (shop == null)
+                throw new ShopManagerException("There is not shop with enough products.");
+
+            return shop;
         }
     }
 }
