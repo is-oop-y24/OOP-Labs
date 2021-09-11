66808069dff8b323445fead4e78be453a730e01a
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-11 16:50:16 +0300

    feat: deleted Supply.AddProduct() overloading

--- Shops/Services/ProductsManagement/Supply.cs
+++ Shops/Services/ProductsManagement/Supply.cs
@@ -10,12 +10,7 @@ namespace Shops
         public Supply() { }
 
         public ReadOnlyCollection<ProductSupply> ProductSupplies => _products.AsReadOnly();
-
-        public void AddProduct(Product product, int quantity, int worth)
-        {
-            _products.Add(new ProductSupply(product, quantity, worth));
-        }
-
+        
         public void AddProduct(ProductSupply productSupply)
         {
             _products.Add(productSupply);
