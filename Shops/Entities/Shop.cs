a44df9d1da3c6e5d947597e90437e7a39351d4b1
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-11 18:09:16 +0300

    feat: implemented shop

--- Shops/Entities/Shop.cs
+++ Shops/Entities/Shop.cs
@@ -1,37 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shops.Tools;
 
 namespace Shops
 {
     public class Shop
     {
+
+        private List<Product> _products = new List<Product>();
+        
         public Shop(string name, ShopId shopId)
         {
-            throw new NotImplementedException();
+            Name = name;
+            Id = shopId;
         }
+        
+        public string Name { get; }
+        public ShopId Id { get; }
 
         public Product RegisterProduct(string name)
         {
-            throw new NotImplementedException();
+            if (_products.Exists(product => product.Name == name))
+                throw new ShopManagerException($"The product {name} is already registered.");
+
+            var product = new Product(name);
+            _products.Add(product);
+            return product;
         }
 
         public Product FindProduct(string name)
         {
-            throw new NotImplementedException();
+            return _products.Find(product => product.Name == name);
         }
 
         public Product GetProduct(string name)
         {
-            throw new NotImplementedException();
+            Product product = FindProduct(name);
+            if (product == null)
+                throw new ShopManagerException($"The product {name} is not found.");
+            return product;
         }
 
-        public void GetSupply(Supply supply)
+        public void MakeSupply(Supply supply)
         {
-            throw new NotImplementedException();
+            foreach (ProductSupply productSupply in supply.ProductSupplies)
+            {
+                Product product = GetProduct(productSupply.ProductName);
+                product.Worth = productSupply.Worth;
+                product.Quantity += productSupply.Quantity;
+            }
+        }
+
+        private int CalculateTotalCost(Purchase purchase)
+        {
+            int totalCost = 0;
+            foreach (ProductPurchase productPurchase in purchase.ProductPurchases)
+            {
+                totalCost += productPurchase.Quantity * GetProduct(productPurchase.ProductName).Worth;
+            }
+            
+            return totalCost;
         }
 
         public void BuyProducts(Purchase purchase)
         {
-            throw new NotImplementedException();
+            int totalCost = CalculateTotalCost(purchase);
+            if (totalCost > purchase.Customer.Balance)
+                throw new ShopManagerException("Not enough money to buy.");
+            purchase.Customer.Balance -= totalCost;
+            
+            foreach (ProductPurchase productPurchase in purchase.ProductPurchases)
+            {
+                Product product = GetProduct(productPurchase.ProductName);
+                if (product.Quantity < productPurchase.Quantity)
+                    throw new ShopManagerException($"Product {product.Name} is not enough.");
+                product.Quantity -= productPurchase.Quantity;
+            }
         }
     }
 }
