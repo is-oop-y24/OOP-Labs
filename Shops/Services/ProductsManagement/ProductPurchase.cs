d1106d8034005c9a43fad5777fd6262a065b0fb7
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-11 17:00:04 +0300

    feat: implemented Purchase structure

--- Shops/Services/ProductsManagement/ProductPurchase.cs
+++ Shops/Services/ProductsManagement/ProductPurchase.cs
@@ -2,6 +2,17 @@ namespace Shops
 {
     public class ProductPurchase
     {
+
+        public ProductPurchase(Customer customer, Product product, int quantity)
+        {
+            Customer = customer;
+            Product = product;
+            Quantity = quantity;
+        }
         
+            
+        public Customer Customer { get; }
+        public Product Product { get; }
+        public int Quantity { get; }
     }
 }
