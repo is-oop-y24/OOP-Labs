4a0a22c55587ba330a54122f9686022bf24d4ace
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-12 11:16:29 +0300

    test: defined tests

--- Shops.Tests/ShopManagerTests.cs
+++ Shops.Tests/ShopManagerTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Shops;
+using Shops.Tools;
 
 namespace Shops.Tests
 {
@@ -16,14 +17,52 @@ namespace Shops.Tests
         [Test]
         public void MakeSupply_ProductsAreAvailableToBuy()
         {
-            
+            Assert.Fail();
         }
 
         [Test]
         public void ChangeProductWorth_WorthHasBeenChanged()
         {
-            
+            Assert.Fail();
         }
-        
+
+        [Test]
+        public void FindMostProfitableShop_FoundCorrectShop()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void FindMostProfitableShop_ShopWithEnoughProductsNotExists_ExceptionThrown()
+        {
+            Assert.Catch<ShopManagerException>(() =>
+            {
+                
+            });
+        }
+
+        [Test]
+        public void BuyProducts_MoneyTransferredAndProductQuantitiesDecreased()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void BuyProducts_NotEnoughMoney_ExceptionThrown()
+        {
+            Assert.Catch<ShopManagerException>(() =>
+            {
+                
+            });
+        }
+
+        [Test]
+        public void BuyProducts_NotEnoughProducts_ExceptionThrown()
+        {
+            Assert.Catch<ShopManagerException>(() =>
+            {
+                
+            });
+        }
     }
 }
