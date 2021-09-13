f7c1089b1980ab5b80d372410163a7f2f9e236ff
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-13 20:43:40 +0300

    feat: implemented ConsoleUI

---
+++ Shops/Client/ConsoleUI.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Shops
+{
+    public class ConsoleUI : IUserInterface
+    {
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
