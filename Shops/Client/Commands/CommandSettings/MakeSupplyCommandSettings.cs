db1b08c688b8ec812e4d65444137b1c2445911db
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-15 11:27:58 +0300

    feat: implemented MakeSupplyCommand

--- Shops/Client/Commands/CommandSettings/MakeSupplyCommandSettings.cs
+++ Shops/Client/Commands/CommandSettings/MakeSupplyCommandSettings.cs
@@ -1,5 +1,10 @@
+using Spectre.Console.Cli;
+
 namespace Shops.Commands
 {
-    public class MakeSupplyCommandSettings
-    { }
+    public class MakeSupplyCommandSettings : CommandSettings
+    {
+        [CommandArgument(0, "[shopName]")]
+        public int ShopId { get; init; }
+    }
 }
--- Shops/Client/Commands/MakeSupplyCommand.cs
+++ Shops/Client/Commands/MakeSupplyCommand.cs
@@ -1,5 +1,24 @@
+using Spectre.Console.Cli;
+
 namespace Shops.Commands
 {
-    public class MakeSupplyCommand
-    { }
+    public class MakeSupplyCommand : Command<MakeSupplyCommandSettings>
+    {
+        private IShopManager _shopManager;
+        private IUserInterface _userInterface;
+
+        public MakeSupplyCommand(IShopManager shopManager, IUserInterface userInterface)
+        {
+            _shopManager = shopManager;
+            _userInterface = userInterface;
+        }
+        
+        public override int Execute(CommandContext context, MakeSupplyCommandSettings settings)
+        {
+            Shop shop = _shopManager.GetShop(new ShopId(settings.ShopId));
+            shop.MakeSupply(shop.CurrentSupply);
+            _userInterface.WriteLine($"Supply in shop {shop.Name} is successfully made.");
+            return 0;
+        }
+    }
 }
