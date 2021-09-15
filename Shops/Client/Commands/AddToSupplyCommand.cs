027e5c53ca0a95e26463bc6e8e4f2d9692ef649e
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-15 11:20:16 +0300

    feat: implemented AddToSupplyCommand

--- Shops/Client/Commands/AddToSupplyCommand.cs
+++ Shops/Client/Commands/AddToSupplyCommand.cs
@@ -1,5 +1,25 @@
+using Spectre.Console.Cli;
+
 namespace Shops.Commands
 {
-    public class AddToSupplyCommand
-    { }
+    public class AddToSupplyCommand : Command<AddToSupplyCommandSettings>
+    {
+        private IShopManager _shopManager;
+        private IUserInterface _userInterface;
+
+        public AddToSupplyCommand(IShopManager shopManager, IUserInterface userInterface)
+        {
+            _shopManager = shopManager;
+            _userInterface = userInterface;
+        }
+
+        public override int Execute(CommandContext context, AddToSupplyCommandSettings settings)
+        {
+            Shop shop = _shopManager.GetShop(new ShopId(settings.ShopId));
+            shop.CurrentSupply.AddProduct(new ProductSupply(
+                shop.GetProduct(settings.ProductName), settings.Quantity, settings.ProductWorth));
+            _userInterface.WriteLine($"Product {settings.ProductName} is successfully added to {shop.Name} supply");
+            return 0;
+        }
+    }
 }
