89e41164a56faceb6bad9fe43740966e8e9ca470
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-18 22:45:22 +0300

    feat: implemented PurchaseListCommand

--- Shops/Client/Commands/PurchaseListCommand.cs
+++ Shops/Client/Commands/PurchaseListCommand.cs
@@ -1,5 +1,32 @@
+using Spectre.Console;
+using Spectre.Console.Cli;
+
 namespace Shops.Commands
 {
-    public class PurchaseListCommand
-    { }
+    public class PurchaseListCommand : Command<PurchaseListCommandSettings>
+    {
+        private readonly IShopManager _shopManager;
+        private readonly IUserInterface _userInterface;
+        private readonly Customer _customer;
+
+        public PurchaseListCommand(IShopManager shopManager, IUserInterface userInterface, Customer customer)
+        {
+            _shopManager = shopManager;
+            _userInterface = userInterface;
+            _customer = customer;
+        }
+
+        public override int Execute(CommandContext context, PurchaseListCommandSettings settings)
+        {
+            var table = new Table();
+            table.AddColumn("Product ID");
+            table.AddColumn("Supply quantity");
+            foreach (ProductPurchase productPurchase in _customer.CurrentPurchase.ProductPurchases)
+            {
+                table.AddRow(productPurchase.ProductId.GetId().ToString(),
+                    productPurchase.Quantity.ToString());
+            }
+            return 0;
+        }
+    }
 }
