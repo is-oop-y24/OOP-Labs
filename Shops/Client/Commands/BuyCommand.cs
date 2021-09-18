869099f56478183cc8285f5928b894f261ccb3ab
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-18 22:49:42 +0300

    feat: implemented BuyProductCommand

--- Shops/Client/Commands/BuyCommand.cs
+++ Shops/Client/Commands/BuyCommand.cs
@@ -1,5 +1,25 @@
+using Spectre.Console.Cli;
+
 namespace Shops.Commands
 {
-    public class BuyCommand
-    { }
+    public class BuyCommand : Command<BuyCommandSettings>
+    {
+        private readonly IShopManager _shopManager;
+        private readonly IUserInterface _userInterface;
+        private readonly Customer _customer;
+
+        public BuyCommand(IShopManager shopManager, IUserInterface userInterface, Customer customer)
+        {
+            _shopManager = shopManager;
+            _userInterface = userInterface;
+            _customer = customer;
+        }
+
+        public override int Execute(CommandContext context, BuyCommandSettings settings)
+        {
+            Shop shop = _shopManager.GetShop(new ShopId(settings.ShopId));
+            shop.BuyProducts(_customer.CurrentPurchase);
+            return 0;
+        }
+    }
 }
--- Shops/Client/Commands/CommandSettings/BuyCommandSettings.cs
+++ Shops/Client/Commands/CommandSettings/BuyCommandSettings.cs
@@ -1,5 +1,10 @@
+using Spectre.Console.Cli;
+
 namespace Shops.Commands
 {
-    public class BuyCommandSettings
-    { }
+    public class BuyCommandSettings : CommandSettings
+    {
+        [CommandArgument(0, "[shopId]")] 
+        public int ShopId { get; set; }
+    }
 }
