bddc5cd9fca3c3bbf099bca3f0614970746e21bb
Mikhail <misha.rukovishnikow@yandex.ru>
2021-09-20 10:11:32 +0300

    fix: moved worth validation to ProductSupply constructor

--- Shops/BusinessLogic/Services/ProductsManagement/ProductSupply.cs
+++ Shops/BusinessLogic/Services/ProductsManagement/ProductSupply.cs
@@ -5,25 +5,18 @@ namespace Shops
     public class ProductSupply
     {
         private int _quantity;
-        private int _worth;
 
         public ProductSupply(ProductId productId, int quantity, int worth)
         {
+            if (worth < 0)
+                throw new ShopManagerException("Product worth cannot be a negative number");
+
             ProductId = productId;
             Quantity = quantity;
             Worth = worth;
         }
 
-        public int Worth
-        {
-            get => _worth;
-            private init
-            {
-                if (value < 0)
-                    throw new ShopManagerException("Product worth cannot be a negative number");
-                _worth = value;
-            }
-        }
+        public int Worth { get; }
 
         public int Quantity
         {
