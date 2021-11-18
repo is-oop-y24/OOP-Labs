3eeef8ea3d723b75bb4e20b4f956d96143540dfc
Mikhail <misha.rukovishnikow@yandex.ru>
2021-11-18 22:13:11 +0300

    feat: implemented DbBankRepository

---
+++ Banks.BusinessLogic/Data/DbBankRepository.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using Banks.BusinessLogic.Tools;
+using Kfc.Utility.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Banks.BusinessLogic.Data
+{
+    public class DbBankRepository : IBankRepository
+    {
+        private readonly BankContext _bankContext;
+
+        public DbBankRepository(BankContext bankContext)
+        {
+            _bankContext = bankContext;
+        }
+        
+        public void AddBank(Bank bank)
+        {
+            bank.ThrowIfNull(nameof(bank));
+            _bankContext.Banks.Add(bank);
+            _bankContext.SaveChanges();
+        }
+
+        public List<Bank> GetBanks()
+        {
+            return _bankContext.Banks
+                .Include(bank => bank.Accounts)
+                .Include(bank => bank.Clients)
+                .Include(bank => bank.Transactions)
+                .ToList();
+        }
+
+        public Bank GetBank(int id)
+        {
+            return GetBanks().Find(bank => bank.Id == id) 
+                   ?? throw new BankException("Bank doesnt exist.");
+        }
+    }
+}
---
+++ Banks.BusinessLogic/Data/IBankRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Banks.BusinessLogic.Data
+{
+    public interface IBankRepository
+    {
+        void AddBank(Bank bank);
+        Bank GetBank(int id);
+        List<Bank> GetBanks();
+    }
+}
