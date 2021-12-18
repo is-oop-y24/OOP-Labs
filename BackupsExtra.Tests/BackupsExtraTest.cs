eb9083f78a9ef954bc3de0b7e6d7abb871ea4f30
Mikhail <misha.rukovishnikow@yandex.ru>
2021-12-18 19:18:23 +0300

    feat: created tests

---
+++ BackupsExtra.Tests/BackupsExtraTest.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Backups;
+using BackupsExtra.Services.Implementations.ExcessPointsChoosers;
+using BackupsExtra.Services.Implementations.JobSavers;
+using BackupsExtra.Services.Services;
+using NUnit.Framework;
+
+namespace BackupsExtra.Tests
+{
+    public class BackupsExtraTest
+    {
+        private IExtraBackupService _extraBackupService;
+        private IExtraJobBuilder _defaultBuilder;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _extraBackupService = new ExtraBackupService(new JobSaver());
+        }
+
+        [Test]
+        public void CountExcessChooserGetExcessPoints_RightPointsNumberReturned()
+        {
+            const int maxCount = 3;
+            const int currentCount = 5;
+            IExcessPointsChooser chooser = new CountPointChooser(maxCount);
+
+            const string pointPath = "";
+            DateTime date = DateTime.Now;
+            var storages = new List<IStorage>();
+
+            var points = new List<RestorePoint>();
+            foreach (int pointNum in Enumerable.Range(1, currentCount))
+            {
+                points.Add(new RestorePoint(date, pointPath, storages));
+                date += TimeSpan.FromDays(1);
+            }
+
+            List<RestorePoint> excessPoints = chooser.ChoosePoints(points);
+            Assert.AreEqual(points.Count - maxCount, excessPoints.Count);
+        }
+
+        [Test]
+        public void DateExcessChooserGetExcessPoints_RightPointsNumReturned()
+        {
+            const int oldCount = 3;
+            const int newCount = 3;
+            var maxPeriod = TimeSpan.FromDays(1);
+            DateTime oldDate = DateTime.Now - maxPeriod * 2;
+            DateTime newDate = DateTime.Now - maxPeriod / 2;
+            IExcessPointsChooser chooser = new DatePointChooser(maxPeriod);
+
+            var points = new List<RestorePoint>();
+            const string pointPath = "";
+            var storages = new List<IStorage>();
+            foreach (int pointNum in Enumerable.Range(1, oldCount))
+            {
+                points.Add(new RestorePoint(oldDate, pointPath, storages));
+            }
+
+            foreach (int pointNum in Enumerable.Range(1, newCount))
+            {
+                points.Add(new RestorePoint(newDate, pointPath, storages));
+            }
+
+            List<RestorePoint> excessPoints = chooser.ChoosePoints(points);
+            Assert.AreEqual(oldCount, excessPoints.Count);
+        }
+    }
+}
--- BackupsExtra/Program.cs
+++ BackupsExtra/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Backups;
 using Backups.FileSystem;
 using BackupsExtra.Services.Implementations.ExcessPointsChoosers;
@@ -15,24 +17,23 @@ namespace BackupsExtra
     {
         private static void Main()
         {
-            IFileRepository repository = new LocalRepository(@"C:\Users\79148\Desktop\Test");
-            var builder = new ExtraJobBuilder();
-            builder.SetLogger(new ConsoleLogger(new TimeLogMessageMaker()));
-            builder.SetDestinationPath(@"C:\Users\79148\Desktop\Test");
-            builder.SetFileRepository(repository);
-            builder.SetJobCleaner(new RemoveJobCleaner());
-            builder.SetJobName("Job1");
-            builder.SetPointsRestorer(new OriginalLocationRestorer(repository, new Unarchiver(), new ConsoleLogger(new TimeLogMessageMaker())));
-            builder.SetStoragePacker(new SplitStoragePacker());
-            builder.SetExcessPointsChooser(new DatePointChooser(new TimeSpan(10, 0, 0, 0)));
-
-            IExtraBackupService service = new ExtraBackupService(new JobSaver());
-            ExtraBackupJob job = service.CreateExtraJob(builder);
-            repository.AddFile(new BackupFile(new FileName("File1"), new byte[] { 1, 2, 3 }), string.Empty);
-            job.AddObject(new JobFiles(new List<string> { @"File1" }, "SomeObj"));
-            job.MakeRestorePoint();
-            service.Save(@"C:\Users\79148\Desktop\Test\Settings");
-            var loadedJob = new JobSaver().Load(@"C:\Users\79148\Desktop\Test\Settings\Job1");
+            // IFileRepository repository = new LocalRepository(@"C:\Users\79148\Desktop\Test");
+            // var builder = new ExtraJobBuilder();
+            // builder.SetLogger(new ConsoleLogger(new TimeLogMessageMaker()));
+            // builder.SetDestinationPath(@"C:\Users\79148\Desktop\Test");
+            // builder.SetFileRepository(repository);
+            // builder.SetJobCleaner(new RemoveJobCleaner());
+            // builder.SetJobName("Job1");
+            // builder.SetPointsRestorer(new OriginalLocationRestorer(repository, new Unarchiver(), new ConsoleLogger(new TimeLogMessageMaker())));
+            // builder.SetStoragePacker(new SplitStoragePacker());
+            // builder.SetExcessPointsChooser(new DatePointChooser(new TimeSpan(10, 0, 0, 0)));
+            //
+            // IExtraBackupService service = new ExtraBackupService(new JobSaver());
+            // ExtraBackupJob job = service.CreateExtraJob(builder);
+            // repository.AddFile(new BackupFile(new FileName("File1"), new byte[] { 1, 2, 3 }), string.Empty);
+            // job.AddObject(new JobFiles(new List<string> { @"File1" }, "SomeObj"));
+            // job.MakeRestorePoint();
+            // job.RestoreThePoint(job.RestorePoints.First());
         }
     }
 }
